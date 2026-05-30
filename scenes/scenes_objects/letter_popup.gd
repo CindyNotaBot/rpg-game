d279098b953782fee63830d265f95c062ed53e82
Cindy <xwucindy@gmail.com>
2026-05-30 19:37:42 -0400

    export 6 letter widhth

--- scenes/scenes_objects/letter_popup.gd
+++ scenes/scenes_objects/letter_popup.gd
@@ -1,7 +1,17 @@
 extends CanvasLayer
 
+func _ready():
+	var screen_size = get_viewport().get_visible_rect().size
+	$ColorRect.position = Vector2(-500, -500)
+	$ColorRect.size = screen_size + Vector2(1000, 1000)
+	$TextureRect.position = Vector2.ZERO
+	$TextureRect.size = screen_size
+	
+	
 func _input(event):
 	if event is InputEventMouseButton and event.pressed:
+		get_tree().paused = false
 		queue_free()
 	if event is InputEventKey and event.pressed:
+		get_tree().paused = false
 		queue_free()
